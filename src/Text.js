9e0964393b613221a2f3213754823bd26cfdc297
Adrian Helvik <adrian.helvik@specifique.no>
2019-10-22 21:01:10 +0200

    Made textContent be a normal field to aide debugging

--- src/Text.js
+++ src/Text.js
@@ -2,19 +2,10 @@ import Node from './Node.js'
 
 class Text extends Node {
   #animationFrame = null
-  #value = null
-
-  get textContent() {
-    return this.#value
-  }
-
-  set textContent(value) {
-    this.#value = value
-  }
 
   constructor(text) {
     super()
-    this.#value = text
+    this.textContent = text
   }
 
   reconcile() {
